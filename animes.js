02d7c0e59ff12c8c5338da738a4ad2d53201e4c0
jair1412 <tomalajair77@gmail.com>
2025-12-01 20:41:56 -0500

    Add files via upload

---
+++ animes.js
@@ -0,0 +1,177 @@
+const ANIMES = {
+
+  tomodachi: {
+    titulo: "Tomodachi no Imouto ga Ore ni dake Uzai",
+    episodios: [
+      "https://hglink.to/e/zw27e3ujkceb",
+      "https://hglink.to/e/bkkegvtpbx4z",
+      "https://hglink.to/e/arqezhrhuz59",
+      "https://hglink.to/e/b9md60ijbay1",
+      "https://hglink.to/e/h0vceud9463l",
+      "https://hglink.to/e/jtj2egu6gha0",
+      "https://hglink.to/e/dgmnmv4e6fg0",
+      "https://hglink.to/e/a7vqrtne56p9",
+      "https://hglink.to/e/dqqagpby9yy6"
+    ]
+  },
+
+  sawaranaide: {
+    titulo: "Sawaranaide Kotesashi-kun",
+    episodios: [
+      "https://hglink.to/e/6id58vkshudm",
+      "https://hglink.to/e/afumweg1mvgg",
+      "https://hglink.to/e/kekpyhyz03fc",
+      "https://hglink.to/e/7oru7a7td4pv",
+      "https://hglink.to/e/hsc0ao71qf5a",
+      "https://hglink.to/e/rtak6a5u8tbe",
+      "https://hglink.to/e/bcuzrhcnby8v",
+      "https://hglink.to/e/fj1atyy3xldx",
+      "https://hglink.to/e/9n0yal1gzmk3"
+    ]
+  },
+
+  ansatsusha: {
+    titulo: "Ansatsusha de Aru Ore no Status ga Yuusha yori mo Akiraka ni Tsuyoi no da ga",
+    episodios: [
+      "https://hglink.to/e/iulctvccj14a",  
+      "https://hglink.to/e/4jbqy4ee3d6k",
+      "https://hglink.to/e/6wndeetegjsq",
+      "https://hglink.to/e/1eoexfx3xr3s",
+      "https://hglink.to/e/yqnb982iorm6",
+      "https://hglink.to/e/110zy6hqsnxz",
+      "https://hglink.to/e/ukbkehz35cob",
+      "https://hglink.to/e/wmwnd7q31d8w",
+      "https://hglink.to/e/ux92piolkyvm"
+    ]
+  },
+  sozai: {
+      titulo: "Sozai Saishuka no Isekai Ryokouki",
+      episodios: [
+    "https://hglink.to/e/aias2ueo18a7",
+    "https://hglink.to/e/gb2jdv7voejj",
+    "https://hglink.to/e/gv290ng3dr08",
+    "https://hglink.to/e/j43uriptz8bg",
+    "https://hglink.to/e/ywzoboy6biso",
+    "https://hglink.to/e/x9nbwiax0dma",
+    "https://hglink.to/e/aias2ueo18a7",
+    "https://hglink.to/e/9lzh6qgcv3zf",
+    "https://hglink.to/e/jhrtcxzpbame",
+    "https://hglink.to/e/faq7n0o623rd"
+     ]
+    },
+  tondemo: {
+      titulo: "Tondemo Skill de Isekai Hourou Meshi Season 2",
+      episodios: [
+    "https://hglink.to/e/1jjeylontqe4",
+    "https://hglink.to/e/bviz9y84acp8",
+    "https://hglink.to/e/5d3hrgdsxrog",
+    "https://hglink.to/e/e5juy7san5x4",
+    "https://hglink.to/e/nhb08h2vmvy5",
+    "https://hglink.to/e/d1o5zfgu9yyo",
+    "https://hglink.to/e/zg8bifrhmytl",
+    "https://hglink.to/e/dcb9dah70hq7"
+     ]
+    },
+    chitose: {
+      titulo: "Chitose-kun wa Ramune Bin no Naka",
+      episodios: [
+        "https://hglink.to/e/35oi65nemey6",
+        "https://hglink.to/e/2ktg9g2zzh4e",
+        "https://hglink.to/e/uanx3gyg6bw5",
+        "https://hglink.to/e/kg2sps5ciq23",
+        "https://hglink.to/e/8z6qhe6hgiu2"
+        ]
+      },
+
+  mushoku: {
+      titulo: "Mushoku no Eiyuu: Betsu ni Skill Nanka Iranakatta n da ga",
+      episodios: [
+        "https://hglink.to/e/zgsqzm2wmwwc",
+        "https://hglink.to/e/7yk561btcrko",
+        "https://hglink.to/e/dbxbdbqpl8op",
+        "https://hglink.to/e/4a2jqk6lifvi",
+        "https://hglink.to/e/7qxbolv1jbj4",
+        "https://hglink.to/e/jh3cr9h5djou",
+        "https://hglink.to/e/welor82wj5nx",
+        "https://hglink.to/e/cbcacssqq5od",
+        "https://hglink.to/e/g2onjb178knv",
+        "https://hglink.to/e/1owrxy2yyijd"
+        ]
+      },
+
+      kekkon: {
+        titulo: "Kekkon Yubiwa Monogatari II",
+        episodios: [
+      "https://hglink.to/e/shz8uzmodw9b",
+      "https://hglink.to/e/geapth1dww2c",
+      "https://hglink.to/e/98lxa83vjrh0",
+      "https://hglink.to/e/35srk8ez2eui",
+      "https://hglink.to/e/b95m1d4xpxkt",
+      "https://hglink.to/e/zntwovner163",
+      "https://hglink.to/e/bivg13u2g7vi",
+      "https://hglink.to/e/qjmy98tfuni3",
+      "https://hglink.to/e/oj6crmk02r1d"
+        ]
+      },
+
+  
+    mikata: {
+        titulo: "Mikata ga Yowasugite Hojo Mahou",
+        episodios: [
+          "https://hglink.to/e/0wr1injcsdtp",
+          "https://hglink.to/e/zwgssno5rwam",
+          "https://hglink.to/e/1c1u4a9ry1oo",
+          "https://hglink.to/e/1c1u4a9ry1oo",
+          "https://hglink.to/e/nnsbw7pk91o8",
+          "https://hglink.to/e/tcij6j27pvd9",
+          "https://hglink.to/e/gifencvoh8ks",
+          "https://hglink.to/e/qavcgspvgxzh",
+          "https://hglink.to/e/nkba6y4ind3e"
+        ]
+      },
+
+    bukiyou: {
+        titulo: "Bukiyou na Senpai",
+        episodios: [
+          "https://hglink.to/e/svwj2je3uh3a",
+          "https://hglink.to/e/yuqu85iq4hqr",
+          "https://hglink.to/e/vjb1x4nwpr7w",
+          "https://hglink.to/e/w3chl2fick01",
+          "https://hglink.to/e/ujdrx4ixjx1o",
+          "https://hglink.to/e/09twdtcp3jqa",
+          "https://hglink.to/e/7qmzk52f0jsq",
+          "https://hglink.to/e/iw0hundblvto",
+          "https://hglink.to/e/mcppcy82xcbe"
+        ]
+      },
+
+    mugen: {
+        titulo: "Mugen Gacha",
+        episodios: [
+          "https://hglink.to/e/8ige06mvpiag",
+          "https://hglink.to/e/vgr3gipmfvxz",
+          "https://hglink.to/e/os509huyynym",
+          "https://hglink.to/e/9p8z6byhwjeq",
+          "https://hglink.to/e/2ju01sfemfyf",
+          "https://hglink.to/e/ev9sz8was0op",
+          "https://hglink.to/e/8kzcncyhaz9n",
+          "https://hglink.to/e/wi697un01lmp",
+          "https://hglink.to/e/jwtu3fnuhej0"
+        ]
+      },
+
+    boku: {
+        titulo: "Boku No Hero Academia: Final Season",
+        episodios: [
+          "https://hglink.to/e/qzcmpbh25ys9",
+          "https://hglink.to/e/za29tieldg5y",
+          "https://hglink.to/e/4x21rn3is4jf",
+          "https://hglink.to/e/9cpro0th1v5t",
+          "https://hglink.to/e/wiy595p5nsph",
+          "https://hglink.to/e/t3ktoui1cr30",
+          "https://hglink.to/e/le4nczfs767f",
+          "https://hglink.to/e/gl2sex0ywzlv",
+          "https://hglink.to/e/e6mzpqjamh5w"
+        ]
+      },
+};
